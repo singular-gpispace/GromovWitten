a52d3dde42b12631a6794de7d798bf6611b4398e
blociss <blaucisse@gmail.com>
2023-06-12 13:24:54 +0200

    modified:   docs/makeLocal.jl 	modified:   src/graphe.jl

--- src/tropicalfeynman.jl
+++ src/tropicalfeynman.jl
@@ -3,9 +3,6 @@ module tropicalfeynman
 using Nemo, Graphs, Combinatorics,StatsBase
 
 #ve=[(1, 2), (1,2),(2, 4), (1, 3) ,(3, 4),(3,4)]
- struct graphe
-    edge::Vector
-end
 import Graphs: nv,ne,Edge,dst, src
 export graphe,nv,ne,PolynomialRing,QQ
 export polynomialring,constterm, proterm, propagator, coefterm, partition, preimg, sgn,flip,flipo,
