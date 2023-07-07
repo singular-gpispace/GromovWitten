ed61c6d9d545a631246886bc27080ef9e81e39f8
blociss <blaucisse@gmail.com>
2023-07-07 18:42:36 +0200

    Gromov

--- src/GromovWitten.jl
+++ src/GromovWitten.jl
@@ -11,7 +11,7 @@ export polynomial_ring,constterm, proterm, propagator, coefterm, partition, prei
 feynman_integral_branchtype, feynman_integral_degree,feynman_integral_degree_sum,subt,consttermV, protermV,  flip_signature,signature_and_multiplicities,
 feynman_integral_branchtype_order,feynman_integral_degree_order,lis,filter_term,filter_vector,
 sum_of_divisor_powers,express_as_eisenstein_series,express_as_powers,polynomial_to_matrix,matrix_of_integral,
-solve_polynomial_system,quasi_matrix
+solve_polynomial_system, quasi_matrix
 
 include("graph.jl")
 include("coeftermV.jl")
