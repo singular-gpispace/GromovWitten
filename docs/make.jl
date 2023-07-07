dbdf1eb7ee6ad665a9f2f627903a84683c21b67e
blociss <46294807+blociss@users.noreply.github.com>
2023-07-07 14:25:54 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,18 @@
 using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
-using tropicalfeynman
+using TropicalFeynman
 using Documenter
-DocMeta.setdocmeta!(tropicalfeynman, :DocTestSetup, :(using tropicalfeynman); recursive=true)
+DocMeta.setdocmeta!(TropicalFeynman, :DocTestSetup, :(using TropicalFeynman); recursive=true)
 
 makedocs(;
-    modules=[tropicalfeynman],
+    modules=[TropicalFeynman],
     authors="Ali Traore",
-    repo="https://github.com/singular-gpispace/tropicalfeynman.jl/blob/{commit}{path}#{line}",
-    sitename="tropicalfeynman.jl",
+    repo="https://github.com/singular-gpispace/TropicalFeynman.jl/blob/{commit}{path}#{line}",
+    sitename="TropicalFeynman.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://singular-gpispace.github.io/tropicalfeynman.git",
+        canonical="https://singular-gpispace.github.io/TropicalFeynman.git",
         edit_link="main",
         assets=String[],
     ),
@@ -22,6 +22,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/singular-gpispace/tropicalfeynman.git",
+    repo="github.com/singular-gpispace/TropicalFeynman.git",
     devbranch="main",
 )
