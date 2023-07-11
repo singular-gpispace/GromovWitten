fbb23025aa667a6749dfc7499ac7d8974c20d31a
blociss <46294807+blociss@users.noreply.github.com>
2023-07-11 17:59:09 +0200

    Update coefterm.jl

--- src/coefterm.jl
+++ src/coefterm.jl
@@ -23,12 +23,12 @@
     p=coeff(p,x,L) # compute the coefficients of degree x1^l1,...,xn^ln.
     return p
 end =#
-@doc Markdown.doc"""
+@doc raw"""
    partition(k::Integer, n::Integer)    
 
 This function returns the number of partitions of $n$ into fixed  $k$ parts. 
 # Examples
-
+```julia
 julia> partition(3,4)
 15-element Vector{Vector{Int64}}:
  [4, 0, 0]
@@ -46,6 +46,7 @@ julia> partition(3,4)
  [0, 2, 2]
  [0, 1, 3]
  [0, 0, 4]
+```
 """
 function partition(n::Integer,k::Integer)
     if(k==0)
