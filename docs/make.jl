7127c42315fd10e546047635ed481d0ab7ec4fd6
blociss <46294807+blociss@users.noreply.github.com>
2023-07-12 12:17:36 +0200

    # Add docs/src/ to LOAD_PATH
    # Add docs/src/ to LOAD_PATH

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
-# push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "docs", "src"))  # Add docs/src/ to LOAD_PATH
 using GromovWitten
 using Documenter
 DocMeta.setdocmeta!(GromovWitten, :DocTestSetup, :(using GromovWitten); recursive=true)
