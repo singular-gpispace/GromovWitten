d35e0446f9837038d2bca6f60204d8141e9f26e7
blociss <blaucisse@gmail.com>
2023-07-13 13:09:14 +0200

    test

--- src/graph.jl
+++ src/graph.jl
@@ -12,8 +12,18 @@ nv(G::graph)=nv(DiGraph(Edge.(G.edge)))
 ne(G::graph)=length(Edge.(G.edge))
 
 function polynomial_ring(G::graph, x::String, q::String, z::String)
-    return @polynomial_ring(QQ, x[1:nv(G)], q[1:ne(G)], z[1:nv(G)])
+    ee=Edge.(G.edge)
+    if length(ee)==0
+        throw(DomainError(G,"G must be non empty graph"))
+    else
+        return @polynomial_ring(QQ, x[1:nv(G)], q[1:ne(G)], z[1:nv(G)])
+    end
 end
 function polynomial_ring(G::graph, x::String, q::String)
-    return @polynomial_ring(QQ, x[1:nv(G)], q[1:ne(G)])
+    ee=Edge.(G.edge)
+    if length(ee)==0
+        throw(DomainError(G,"G must be non empty graph"))
+    else
+        return @polynomial_ring(QQ, x[1:nv(G)], q[1:ne(G)])
+    end
 end
