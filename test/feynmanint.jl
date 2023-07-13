501cd72d73b5871207f3dc54935a5add45ba57ab
blociss <blaucisse@gmail.com>
2023-07-13 11:44:59 +0200

    test

--- test/feynmanint.jl
+++ test/feynmanint.jl
@@ -4,7 +4,6 @@
     R,x,q=polynomial_ring(G,"x","q")
     a=[2,0,0,1]
     o=[1,2,3]
-    @test loopterm(q[1],2)==3*q[1]^4
     @test feynman_integral_branch_type_order(x,q,G,a,o)==3*q[1]^4*q[4]^2
     @test feynman_integral_branch_type(x,q,G,a,l=[0,0,0])==6*q[1]^4*q[4]^2
     @test feynman_integral_degree_order(x,q,G,o,3,l=[0,0,0])==3*q[1]^4*q[4]^2+ q[1]^2*q[2]^2*q[3]^2+ q[1]^2*q[2]^2*q[4]^2+ q[1]^2*q[3]^2*q[4]^2+ 9*q[1]^2*q[4]^4
