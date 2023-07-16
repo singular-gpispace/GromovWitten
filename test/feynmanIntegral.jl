9253f04b678555ab2d5a6ffb7bbc140f5792b93f
blociss <blaucisse@gmail.com>
2023-07-16 17:25:10 +0200

    test

--- test/feynmanIntegral.jl
+++ test/feynmanIntegral.jl
@@ -12,7 +12,10 @@
     @test sum_of_coeff(feynman_integral_degree(x,q,G,4))==25344
     @test feynman_integral_degree_sum_order(x,q,G,Ω,[1,3])==12*q[2]^6 + 76*q[2]^4*q[3]^2 + 4*q[2]^4*q[5]^2 + 4*q[2]^4*q[6]^2 + 76*q[2]^2*q[3]^4 + 16*q[2]^2*q[3]^2*q[5]^2 + 16*q[2]^2*q[3]^2*q[6]^2 + 4*q[2]^2*q[3]^2 + 12*q[3]^6 + 4*q[3]^4*q[5]^2 + 4*q[3]^4*q[6]^2
     @test feynman_integral_degree_sum_order(x,q,G,Ω,3)==12*q[2]^6 + 76*q[2]^4*q[3]^2 + 4*q[2]^4*q[5]^2 + 4*q[2]^4*q[6]^2 + 76*q[2]^2*q[3]^4 + 16*q[2]^2*q[3]^2*q[5]^2 + 16*q[2]^2*q[3]^2*q[6]^2 + 4*q[2]^2*q[3]^2 + 12*q[3]^6 + 4*q[3]^4*q[5]^2 + 4*q[3]^4*q[6]^2
-    @test substitute(q,feynman_integral_degree_sum(x,q,G,[1,4]))==25344*q[1]^8 + 1792*q[1]^6 + 32*q[1]^4
+    @test feynman_integral_degree_sum(x,q,G,[1,2])==8*q[1]^4 + 8*q[2]^2*q[3]^2 + 8*q[4]^4 + 8*q[5]^2*q[6]^2
+    @test feynman_integral_degree_sum_order(x,q,G,Ω,[1,2])==4*q[2]^2*q[3]^2
     @test substitute(q,feynman_integral_degree_sum(x,q,G,4))==25344*q[1]^8 + 1792*q[1]^6 + 32*q[1]^4
     @test  substitute(q,0)==0
+    p=6*q[1]^2*q[2]^2*q[3]^4 + 12*q[1]^2*q[2]^2*q[3]^2*q[4]^2 + 6*q[1]^2*q[2]^2*q[4]^4 + 56*q[1]^2*q[3]^6 + 6*q[1]^2*q[3]^4*q[4]^2 + 6*q[1]^2*q[3]^2*q[4]^4 + 56*q[1]^2*q[4]^6 + q[1]
+    @test  filter_term(p,q[1],1)==q[1]
 end
