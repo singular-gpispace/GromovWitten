003fa839119953b9a4f4ea7d3d4633ff4681dc7b
blociss <46294807+blociss@users.noreply.github.com>
2023-10-24 17:35:37 +0200

    Update GromovWitten.jl

--- src/GromovWitten.jl
+++ src/GromovWitten.jl
@@ -4,16 +4,16 @@ GromovWitten  is a package for computing Gromov-Witten invariant via Feynman Int
 module GromovWitten
 # Write your package code here.
 using Nemo, Graphs, Combinatorics,StatsBase
-import Nemo: vars
 export  QQFieldElem,@polynomial_ring
 import Graphs: nv,ne,Edge,dst, src
-export graph,nv,ne,PolynomialRing,QQ
+export FeynmanGraph,nv,ne,PolynomialRing,QQ,FeynmanIntegral
 export polynomial_ring,constterm, proterm, propagator, coefterm, partition, preimg, substitute,flip,signature_and_multiplicities_order,
 feynman_integral_branch_type, feynman_integral_degree,feynman_integral_degree_sum_order,feynman_integral_degree_sum,sum_of_coeff,loopterm,  flip_signature,signature_and_multiplicities,
 feynman_integral_branch_type_order,feynman_integral_degree_order,lis,filter_term,filter_vector,
 sum_of_divisor_powers,express_as_eisenstein_series,express_as_powers,polynomial_to_matrix,matrix_of_integral,
 solve_polynomial_system, quasi_matrix,sfunction,inv_sfunction
 export eisenstein_series,quasimodular_form
+export cache_integral_result, get_integral_from_cache, feynman_integral_branch_type_cache, feynman_integral_degree_cache,feynman_integral_degree_sum_cache
 
 include("graph.jl")
 include("coeftermV.jl")
@@ -21,4 +21,5 @@ include("coefterm.jl")
 include("feynmanIntegral.jl")
 include("propagator.jl")
 include("quasimodular.jl")
+include("hashtable.jl")
 end
