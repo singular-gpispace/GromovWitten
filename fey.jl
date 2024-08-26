aee872e84272a59f04c78b3ad69f3d8310d93325
blociss <blaucisse@gmail.com>
2024-08-26 16:05:58 +0200

    up

--- fey.jl
+++ fey.jl
@@ -1,6 +1,9 @@
 using GromovWitten
 ve = [(1, 3), (1, 2), (1, 2), (2, 4), (3, 4), (3, 4)]
+ve = [(1, 2), (1, 2), (1, 3), (2, 4), (3, 4), (3, 5), (4, 6), (5, 6), (5, 6)]
+ve = [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
 G = FeynmanGraph(ve)
 F = FeynmanIntegral(G)
-d = 14
+d = 8
 @time substitute(feynman_integral_degree(F, d))
+
